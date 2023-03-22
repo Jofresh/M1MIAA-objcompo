86c05a3fbbdfbf6a37060fbf699af8498256569e
Geoffray Gaborit <gaborit.geoffray@gmail.com>
2023-03-22 22:10:28 +0100

    feat: add ComposantDS

---
+++ ComposantDS/src/sources/ComponentInterneImpl.java
@@ -0,0 +1,19 @@
+package sources;
+
+import Types.TypeService1;
+import Types.TypeService2;
+
+public class ComponentInterneImpl implements Runnable, TypeService1 {
+	@Override
+	public void run() {
+		
+	}
+	
+	public void setLien(TypeService2 s) {
+		
+	}
+	
+	public void unsetLien(TypeService2 s) {
+		
+	}
+}
